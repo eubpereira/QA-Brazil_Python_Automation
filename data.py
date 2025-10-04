f42bf037b453af9e3066d0d1cddce6256990a2ab
Bernardo Pereira <eubpereira@gmail.com>
2025-10-04 10:41:38 -0300

    atualização sprint7

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-2fe5f37e-9213-4f04-b1e8-53b39391d245.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO  =  	'1300 1st St'
+PHONE_NUMBER = 	'+1 123 123 12 12'
+CARD_NUMBER = 	'1234 5678 9100'
+CARD_CODE = 	'1111'
+MESSAGE_FOR_DRIVER = 	'Pare no bar de sucos'
--- main.py
+++ main.py
@@ -1,2 +1,57 @@
+import data
+import helpers
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
+    def test_set_route(self):
+        print("Função criada para definir a rota")
+        # Adicionar em S8
+        pass
+
+    def test_select_plan(self):
+        print("Função criada para definir o plano de carro")
+        # Adicionar em S8
+        pass
+
+    def test_fill_phone_number(self):
+        print("Função criada para inserir o número de telefone")
+        # Adicionar em S8
+        pass
+
+    def test_fill_card(self):
+        print("Função criada para inserir o número do cartão")
+        # Adicionar em S8
+        pass
+
+    def test_comment_for_driver(self):
+        print("Função criada para inserir um comentário ao motorista")
+        # Adicionar em S8
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        print("Função criada para pedir cobertor e lençóis")
+        # Adicionar em S8
+        pass
+
+    def test_order_2_ice_creams(self):
+        print("Função criada para pedir sorvetes")
+        # Adicionar em S8
+        pass
+
+    def test_car_search_model_appears(self):
+        print("Função criada para procurar o modelo do carro")
+        # Adicionar em S8
+        pass
+
+    def test_order_2_ice_creams(self):
+        numbers_of_icecreams = 2
+        for count in range(numbers_of_icecreams):
+            print("Função criada para pedir 2 sorvetes")
+            # Adicionar em S8
+        pass
