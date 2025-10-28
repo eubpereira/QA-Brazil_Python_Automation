8bb15e89e3dba92097e7b61d67fa76b1240c0cec
Bernardo Pereira <eubpereira@gmail.com>
2025-10-28 17:44:27 -0300

    atualização projeto sprint8

--- main.py
+++ main.py
@@ -1,57 +1,111 @@
+from pages import UrbanRoutesPage
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import data
 import helpers
+import time
+
 
 class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
+        from selenium.webdriver import DesiredCapabilities
+        capabilities = DesiredCapabilities.CHROME
+        capabilities["goog:loggingPrefs"] = {'performance': 'ALL'}
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(5)
+
         if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
             print("Conectado ao servidor Urban Routes")
         else:
             print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
 
+
     def test_set_route(self):
-        print("Função criada para definir a rota")
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        assert routes_page.get_from_location_value() == data.ADDRESS_FROM
+        assert routes_page.get_to_location_value() == data.ADDRESS_TO
+
+
 
     def test_select_plan(self):
-        print("Função criada para definir o plano de carro")
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_taxi_option()
+        routes_page.click_comfort_icon()
+        assert routes_page.click_comfort_active()
+
 
     def test_fill_phone_number(self):
-        print("Função criada para inserir o número de telefone")
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_taxi_option()
+        routes_page.click_comfort_icon()
+        routes_page.click_number_text(data.PHONE_NUMBER)
+        assert data.PHONE_NUMBER in routes_page.numero_confirmado()
+
 
     def test_fill_card(self):
-        print("Função criada para inserir o número do cartão")
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_taxi_option()
+        routes_page.click_comfort_icon()
+        routes_page.click_add_cartao(data.CARD_NUMBER, data.CARD_CODE)
+        assert "Cartão" in routes_page.confirm_cartao()
+        time.sleep(3)
 
     def test_comment_for_driver(self):
-        print("Função criada para inserir um comentário ao motorista")
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_taxi_option()
+        routes_page.click_comfort_icon()
+        routes_page.add_comentario(data.MESSAGE_FOR_DRIVER)
+        assert data.MESSAGE_FOR_DRIVER in routes_page.comment_confirm()
+
 
     def test_order_blanket_and_handkerchiefs(self):
-        print("Função criada para pedir cobertor e lençóis")
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_taxi_option()
+        routes_page.click_comfort_icon()
+        routes_page.switch_cobertor()
+        assert routes_page.switch_cobertor_active() is True
+        time.sleep(2)
 
     def test_order_2_ice_creams(self):
-        print("Função criada para pedir sorvetes")
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_taxi_option()
+        routes_page.click_comfort_icon()
+        for _ in range(2):
+            routes_page.add_ice()
+        assert int(routes_page.qnt_sorvete()) == 2
+
 
     def test_car_search_model_appears(self):
-        print("Função criada para procurar o modelo do carro")
-        # Adicionar em S8
-        pass
+        self.driver.get(data.URBAN_ROUTES_URL)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        routes_page.click_taxi_option()
+        routes_page.click_comfort_icon()
+        routes_page.click_number_text(data.PHONE_NUMBER)
+        routes_page.click_add_cartao(data.CARD_NUMBER, data.CARD_CODE)
+        routes_page.add_comentario(data.MESSAGE_FOR_DRIVER)
+        routes_page.call_taxi()
+        assert "Buscar carro" in routes_page.pop_up_show()
+        time.sleep(10)
 
-    def test_order_2_ice_creams(self):
-        numbers_of_icecreams = 2
-        for count in range(numbers_of_icecreams):
-            print("Função criada para pedir 2 sorvetes")
-            # Adicionar em S8
-        pass
+    @classmethod
+    def teardown_class(cls):
+        cls.driver.quit()
